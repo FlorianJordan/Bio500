92644598a1dc3ae3289c07dfe2660d7080d7d19f
Simgreg <simong.22@hotmail.com>
2022-03-29 13:26:02 -0400

    Ajout table SQL

--- Code_equipe.R
+++ Code_equipe.R
@@ -6,6 +6,8 @@
 
 #install.packages("dyplr")
 library(dplyr)
+#install.packages("RSQLite")
+library(RSQLite)
 ##### Chercher les données #####
 
 noeuds_amelie<-read.csv("noeuds_amelie.csv", sep=";")
@@ -50,11 +52,16 @@ noeuds_cvl<-noeuds_cvl[,-1]
 
 cours_cvl<-rename(cours_cvl,sigle=Sigle)
 cours_fxc<-rename(cours_fxc,credits=credit)
+
 collaborations_amelie<-rename(collaborations_amelie,sigle=cours)
 collaborations_anthonystp<-rename(collaborations_anthonystp,sigle=cours)
 collaborations_cvl<-rename(collaborations_cvl,sigle=cours)
 collaborations_dp<-rename(collaborations_dp,sigle=cours)
 collaborations_martineau<-rename(collaborations_martineau,sigle=cours)
+
+colnames(noeuds_amelie)<-c("nom_prenom","annee_debut","session_debut","programme","coop")
+colnames(collaborations_amelie)<-c("etudiant1","etudiant2","sigle","date")
+colnames(cours_amelie)<-c("sigle","credits","obligatoire","laboratoire","libre")
 
 ##### Fusionner les fichiers ####
 
@@ -65,4 +72,88 @@ data_cours
 data_collaborations<-bind_rows(collaborations_amelie,collaborations_anthonystp,collaborations_cvl,collaborations_dp,collaborations_fxc,collaborations_jbca,collaborations_martineau,collaborations_alexis,collaborations_ilmdph)
 data_collaborations
 
-#### Enlever les doublons ####
+#### Enlever les doublons #### sujet à changement ####
+
+cours<-data_cours[!duplicated(data_cours$sigle),]
+cours
+
+
+collaborations<-distinct(data_collaborations)
+collaborations
+
+noeuds<-data_noeuds[!duplicated(data_noeuds$nom_prenom),]
+noeuds
+
+#### Enlever les anciennes tables SQL ####
+
+con<-dbConnect(SQLite(),dbname="attributs.db")
+dbSendQuery(con,"DROP TABLE cours;")
+
+#### Creation des tables SQL ####
+
+tbl_noeuds <- "
+CREATE TABLE noeuds (
+  nom_prenom      VARCHAR(50),
+  annee_debut      DATE(4),
+  session_debut CHAR(1),
+  programme       VARCHAR(20),
+  coop        BOOLEAN(1),
+  PRIMARY KEY (nom_prenom)
+);"
+dbSendQuery(con, tbl_noeuds)
+
+tbl_cours <- "
+CREATE TABLE cours (
+sigle   CHAR(6) NOT NULL,
+credits       INTEGER(1) ,
+obligatoire     BOOLEAN(1),
+laboratoire       BOOLEAN(1),
+distance   BOOLEAN(1),
+groupes   BOOLEAN(1),
+libre   BOOLEAN(1),
+PRIMARY KEY (sigle)
+);"
+dbSendQuery(con, tbl_cours)
+
+tbl_collaborations <- "
+CREATE TABLE collaborations (
+  etudiant1     VARCHAR(50),
+  etudiant2     VARCHAR(50),
+  sigle   CHAR(6),
+  date    DATE(3),
+  PRIMARY KEY (etudiant1, etudiant2, sigle,date),
+  FOREIGN KEY (etudiant1) REFERENCES noeuds(nom_prenom),
+  FOREIGN KEY (etudiant2) REFERENCES noeuds(nom_prenom),
+  FOREIGN KEY (sigle) REFERENCES cours(sigle)
+);"
+dbSendQuery(con, tbl_collaborations)
+dbListTables(con)
+
+
+dbWriteTable(con, append = TRUE, name = "noeuds", value = noeuds, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "cours", value = cours, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "collaborations", value = collaborations, row.names = FALSE)
+
+sql_requete <- "
+SELECT etudiant1 as etudiant, count(etudiant2) as liens
+FROM collaborations
+GROUP BY etudiant
+ORDER BY liens
+"
+liens <- dbGetQuery(con,sql_requete)
+liens
+
+sql_requete2 <- "
+SELECT etudiant1, etudiant2, count(sigle) as liens
+FROM collaborations
+GROUP BY etudiant1, etudiant2
+ORDER BY liens
+"
+liens_paire <- dbGetQuery(con,sql_requete2)
+liens_paire
+
+#### Calcul de paramètres des liens ####
+
+mean(liens$liens)
+var(liens$liens)
+
