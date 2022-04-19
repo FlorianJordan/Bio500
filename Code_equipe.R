d9d124aba3eaa0dcea5dd879d20ea19c576b0156
florian JORDAN <florianjordan@MacBook-Pro-de-florian.local>
2022-04-19 09:44:36 -0400

    push

--- Code_equipe.R
+++ Code_equipe.R
@@ -2,7 +2,7 @@
 #getwd()
 #setwd()
 #
-#pour flo # setwd("~/Desktop/dossier_mere/universite/Session_6/method_comp/Bio500/donnees_BIO500")
+#pour flo # setwd("~/Desktop/dossier_mere/universite/Session_6/method_comp/Bio500")
 #pour frank: setwd("C:/Users/Francis/Desktop/BIO500/Bio500/donnees_BIO500")
 #pour marge : setwd("C:\Users\margu\Documents\Bio500\donnees_BIO500")
 
