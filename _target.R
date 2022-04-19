4d18e7d6e87ba276df8a247f803c43fd0d17f105
stpa2707 <astp@live.fr>
2022-04-19 09:18:30 -0400

    ajout

--- _target.R
+++ _target.R
@@ -23,6 +23,15 @@ list(
   tar_target(cours_alexis,read.table("donnees_BIO500/Cours_Alexis_Nadya_Edouard_Penelope.txt",header = T,sep="")),
   tar_target(cours_ilmdph,read.table("donnees_BIO500/cours_IL_MDH_ASP_MB_OL.txt",header = T,sep=";")),
   
+  tar_target(collaborations_amelie,read.csv("data/collaborations_amelie.csv", sep=";")),
+  tar_target(collaborations_anthonystp,read.csv("data/collaborations_anthonystp.csv", sep=";")),
+  tar_target(collaborations_cvl,read.csv("data/collaborations_cvl_jl_jl_mp_xs.csv", sep=";")),
+  tar_target(collaborations_dp,read.csv("data/collaborations_DP-GL-LB-ML-VQ_txt.csv", sep=";")),
+  tar_target(collaborations_fxc,read.csv("data/collaborations_FXC_MF_TC_LRT_WP.csv", sep=";")),
+  tar_target(collaborations_jbca,read.csv("data/collaborations_jbcaldlvjlgr.csv", sep=";")),
+  tar_target(collaborations_martineau,read.csv("data/collaborations_martineau.csv", sep=";")),
+  tar_target(collaborations_alexis,read.csv("data/collaboration_Alexis_Nadya_Edouard_Penelope.csv", sep=";")),
+  tar_target(collaborations_ilmdph,read.csv("data/collaborations_IL_MDH_ASP_MB_OL.csv", sep=";"))
   )
   
 
