40e5311bba24b8743c6cda7b0bc5addf3d354062
florian JORDAN <florianjordan@MacBook-Pro-de-florian.local>
2022-04-19 11:15:05 -0400

    fonction

--- R/fonctions.R
+++ R/fonctions.R
@@ -14,27 +14,14 @@ fonction_noeuds_cvl<-function(x){
   x[,-1]
 }
 
-fonction_cours_cvl<-rename(x,sigle=Sigle)
-fonction_cours_fxc<-rename(x,credits=credit)
-fonction_collaborations_amelie<-rename(x,sigle=cours)
-fonction_collaborations_anthonystp<-rename(x,sigle=cours)
-fonction_collaborations_cvl<-rename(x,sigle=cours)
-fonction_collaborations_dp<-rename(x,sigle=cours)
-fonction_collaborations_martineau<-rename(x,sigle=cours)
+fonction_cours_cvl<-function(x){rename(x,sigle=Sigle)}
+fonction_cours_fxc<-function(x){rename(x,credits=credit)}
+fonction_collaborations_amelie<-function(x){rename(x,sigle=cours)
+  colnames(x)<-c("etudiant1","etudiant2","sigle","date")}
+fonction_collaborations_anthonystp<-function(x){rename(x,sigle=cours)}
+fonction_collaborations_cvl<-function(x){rename(x,sigle=cours)}
+fonction_collaborations_dp<-function(x){rename(x,sigle=cours)}
+fonction_collaborations_martineau<-function(x){rename(x,sigle=cours)}
 
-fonction_noeuds_amelie<-colnames(x)<-c("nom_prenom","annee_debut","session_debut","programme","coop")
-fonction_collaborations_amelie<-colnames(x)<-c("etudiant1","etudiant2","sigle","date")
-
-
-cours_cvl<-rename(cours_cvl,sigle=Sigle)
-cours_fxc<-rename(cours_fxc,credits=credit)
-
-collaborations_amelie<-rename(collaborations_amelie,sigle=cours)
-collaborations_anthonystp<-rename(collaborations_anthonystp,sigle=cours)
-collaborations_cvl<-rename(collaborations_cvl,sigle=cours)
-collaborations_dp<-rename(collaborations_dp,sigle=cours)
-collaborations_martineau<-rename(collaborations_martineau,sigle=cours)
-
-colnames(noeuds_amelie)<-c("nom_prenom","annee_debut","session_debut","programme","coop")
-colnames(collaborations_amelie)<-c("etudiant1","etudiant2","sigle","date")
-colnames(cours_amelie)<-c("sigle","credits","obligatoire","laboratoire","libre")
+fonction_noeuds_amelie<-function(x){colnames(x)<-c("nom_prenom","annee_debut","session_debut","programme","coop")}
+fonction_collaborations_amelie<-function(x){colnames(x)<-c("etudiant1","etudiant2","sigle","date")}
