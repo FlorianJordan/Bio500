ff00ece041eb8d2d41620fe19c4cb714c29247b3
Simgreg <simong.22@hotmail.com>
2022-04-23 19:11:14 -0400

    graph

--- Code_equipe.R
+++ Code_equipe.R
@@ -200,11 +200,10 @@ rk<-rank(deg)
 col.vec<-heat.colors(nrow(m_adj))
 adj<-graph.adjacency(m_adj)
 V(adj)$color = col.vec[rk]
-col.vec<-seq(2, 20, length.out = nrow(m_adj))
+col.vec<-seq(10, 70, length.out = nrow(m_adj))
 V(adj)$size = col.vec[rk]
 
-plot(adj, vertex.label = NA, edge.arrow.mode = 0, layout=layout.kamada.kawai(adj))
-
+plot(adj, vertex.label = NA, edge.arrow.mode = 0, layout=layout.kamada.kawai(adj), rescale=FALSE, ylim=c(-7,7), xlim=c(-7,7))
 
 #### Enlever TSB303 ####
 
