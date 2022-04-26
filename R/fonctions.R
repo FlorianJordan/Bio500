dbfb5f2e071a32f8a9b62d34c02f564573d4da46
florian JORDAN <florianjordan@MacBook-Pro-de-florian.local>
2022-04-25 20:05:16 -0400

    s

--- R/fonctions.R
+++ R/fonctions.R
@@ -32,10 +32,9 @@ fonction_doublons_noeuds<-function(x){x %>% arrange(rowSums(is.na(x)))
   x}
 
 
-fonction_creation_table<-function(noeuds,cours,collaborations){
+fonction_creation_table<-function(x,y,z){
 
   con<-dbConnect(SQLite(),dbname="attributs.db")
-
   dbSendQuery(con,"DROP TABLE collaborations;")
   dbSendQuery(con,"DROP TABLE noeuds;")
   dbSendQuery(con,"DROP TABLE cours;")
@@ -76,11 +75,10 @@ CREATE TABLE collaborations (
   FOREIGN KEY (sigle) REFERENCES cours(sigle)
 );"
   dbSendQuery(con, tbl_collaborations)
-  dbWriteTable(con, append = TRUE, name = "noeuds", value = noeuds, row.names = FALSE)
-  dbWriteTable(con, append = TRUE, name = "cours", value = cours, row.names = FALSE)
-  dbWriteTable(con, append = TRUE, name = "collaborations", value = collaborations, row.names = FALSE)
-  con
-  dbDisconnect()
+  dbWriteTable(con, append = TRUE, name = "noeuds", value = x, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "cours", value = y, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "collaborations", value = z, row.names = FALSE)
+
   }
 
 graph_base<-function(x){
@@ -105,7 +103,7 @@ plot(adj2, vertex.label = NA, edge.arrow.mode = 0, layout=layout.kamada.kawai(ad
 
 
 fonction_requete_tsb303<-function(){
- 
+  con<-dbConnect(SQLite(),dbname="attributs.db")
 sql_requete3 <- "
 SELECT etudiant1,etudiant2,sigle,date
 FROM collaborations WHERE sigle NOT LIKE '%TSB303%'
