e620ddb502a4360ffaa14a81379128f8ce359fe0
florian JORDAN <florianjordan@MacBook-Pro-de-florian.local>
2022-04-26 23:48:23 -0400

    a

--- R/fonctions.R
+++ R/fonctions.R
@@ -80,7 +80,9 @@ function_connection_SQL <- function() {
 fonction_creation_table<-function(con,noeuds,cours,collaborations){
   
   con<-dbConnect(SQLite(), dbname = "attributs.db")
-  
+  dbSendQuery(con,"DROP TABLE collaborations;")
+  dbSendQuery(con,"DROP TABLE noeuds;")
+  dbSendQuery(con,"DROP TABLE cours;")
 
   
   tbl_noeuds <- "
